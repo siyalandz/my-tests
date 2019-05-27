d6b9517a7f1f68ecafc0f6e270d37b27ca3a4e97
Siyanda <siyalandz@gmail.com>
2019-05-27 09:25:13 +0200

    modify my test

--- mostProfitableDepartment.js
+++ mostProfitableDepartment.js
@@ -1,37 +1,24 @@
-function mostProfitableDepartment(depList){
-  var num = [];
-  var total = '';
-  for(var i= 0; i<depList.length; i++){
-    var sales=depList[i].sales;
-    var depart=depList[i].total;
-    if(depList[i].sales > depList[i]){
-      num=sales;
-      total=depart;
-    }
-  }
-  return total;
-}
+function mostProfitableDepartment(listItems) {
+  var highestSales = 0;
+  var profitableDept = '';
+  console.log(listItems);
 
+   for(var i = 0; i < listItems.length; i++) {
+       var listItem = listItems[i];
+     
+       if(listItem.sales > highestSales) {
+           highestSales = listItem.sales;
+           profitableDept = listItem.department;
+       }
+   }
+   //console.log("Profitable Dept: " + profitableDept);
+   //console.log("Highest Sales: " + highestSales);
+   return profitableDept;
+}	
+// var list = [
+// {department: 'hardware', sales: 4500, day: 'Monday'},
+// {department: 'outdoor', sales: 1500, day: 'Monday'},
 
+// ];
 
-//
-// console.log(total[0].department)
-var list = [
-{department: 'hardware', sales: 4500, day: 'Monday'},
-{department: 'outdoor', sales: 1500, day: 'Monday'},
-{department: 'carpentry', sales: 5500, day: 'Monday'},
-{department: 'hardware', sales: 7500, day: 'Tuesday'},
-{department: 'outdoor', sales: 2505, day: 'Tuesday'},
-{department: 'carpentry', sales: 1540, day: 'Tuesday'},
-{department: 'hardware', sales: 1500, day: 'Wednesday'},
-{department: 'outdoor', sales: 8507, day: 'Wednesday'},
-{department: 'carpentry', sales: 8009, day: 'Wednesday'},
-{department: 'hardware', sales: 12000, day: 'Thursday'},
-{department: 'outdoor', sales: 18007, day: 'Thursday'},
-{department: 'carpentry', sales: 6109, day: 'Thursday'},
-{department: 'hardware', sales: 7005, day: 'Friday'},
-{department: 'outdoor', sales: 12006, day: 'Friday'},
-{department: 'carpentry', sales: 16109, day: 'Friday'}
-];
-
-alert(mostProfitableDepartment(list))
+// alert( mostProfitableDepartment(list) )
--- tests/mostProfitableDepartment.test.js
+++ tests/mostProfitableDepartment.test.js
@@ -1,7 +1,17 @@
 describe('most profitable department',function(){
     it('should return highest paid department',function(){
-    assert.deepEqual(mostProfitableDepartment([
+    var departments = [
         {'department': 'hardware', 'sales': 4500, 'day': 'Monday'},
-        {'department': 'outdoor', 'sales': 1500, 'day': 'Monday'}]),['outdoor']);
+        {'department': 'outdoor', 'sales': 1500, 'day': 'Monday'}
+    ]
+
+    assert.deepEqual(mostProfitableDepartment(departments),'hardware');
     });
+
+    it('Should return false when an invalid input',function(){
+        
+    
+        assert.equal(mostProfitableDepartment('departments'), false);
+        });
 });
+
